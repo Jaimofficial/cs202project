d0691a0c85abb344f52d8813df53c39c75b6ead7
Jaimofficial <jaimehta615@gmail.com>
2024-10-21 15:44:05 -0400

    Updated checkers.h

--- checkers.h
+++ checkers.h
@@ -2,6 +2,15 @@
 
 #define _CHECKERS_H_
 
+	class Board {
+	
+		public:
+			void Move(int); //Allows user to move to a specified index
+			bool isPromoted(int); // checks if the checker can move backwards
+		private:
+		vector< vector < char > > Board;
+	
+	};
 
 
 
