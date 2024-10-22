a6c73907e338b484c69ec375552e6510e100f2fe
ThomasDonahue01 <tdonahu3@vols.utk.edu>
2024-10-22 11:56:56 -0400

    added isValidMove function. It's not completed

--- checkers.h
+++ checkers.h
@@ -1,12 +1,14 @@
 #ifndef _CHECKERS_H_
 #define _CHECKERS_H_
 #include <vector>
+
 	class Checkers {
 	
 		public:
 			Checkers();
 			~Checkers();
 			void Move(int); //Allows user to move to a specified index
+			bool isValidMove(std::vector< std::vector < char > >, int pieceToMove[], int whereToMove[], char);
 			bool isPromoted(int); // checks if the checker can move backwards
 			void printBoard(); // print the total board
 		private:
