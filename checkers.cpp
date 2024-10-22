6f4de25ab261ceaeefbe5b7b8f5016841b4d0c1b
Jaimofficial <jaimehta615@gmail.com>
2024-10-21 22:29:12 -0400

    git commit checkers.cpp

--- checkers.cpp
+++ checkers.cpp
@@ -7,14 +7,14 @@ using namespace std;
 Checkers::Checkers() {
 	Board.resize(8);
 	for (int rows = 0; rows < 8; rows++) {
-		Board[rows].resize(8, ' ');
+		Board[rows].resize(8, ' '); // used to create an 8x8 board
 	}
 	for (int i = 0; i < 3; i++) { 
 		for (int j = 0; j < 8; j++) {
 			Board[i][(j + i) % 8] = 'b';
 			Board[7 - i][(j++ + i + 1) % 8] = 'r';
 		}
-	}
+	} // places red and black on the board and staggers them accordingly.
 }
 Checkers::~Checkers() {
 }
