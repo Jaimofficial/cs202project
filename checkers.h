4314eec6f7904ba8b37ed706013b6cec1de6c570
Jaimofficial <jaimehta615@gmail.com>
2024-10-21 22:14:13 -0400

    Added constructor, deconstructor, and print function. Used print to test board.

--- checkers.h
+++ checkers.h
@@ -1,14 +1,16 @@
 #ifndef _CHECKERS_H_
-
 #define _CHECKERS_H_
-
-	class Board {
+#include <vector>
+	class Checkers {
 	
 		public:
+			Checkers();
+			~Checkers();
 			void Move(int); //Allows user to move to a specified index
 			bool isPromoted(int); // checks if the checker can move backwards
+			void printBoard(); // print the total board
 		private:
-		vector< vector < char > > Board;
+				std::vector< std::vector < char > > Board;
 	
 	};
 
