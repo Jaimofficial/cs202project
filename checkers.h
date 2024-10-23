0d56d21e94f865b1b6044599bfe9f9072adb134f
ThomasDonahue01 <tdonahu3@vols.utk.edu>
2024-10-23 11:10:44 -0400

    implementing changes to board after the player has inputted their move

--- checkers.h
+++ checkers.h
@@ -8,7 +8,7 @@
 			Checkers();
 			~Checkers();
 			void Move(int); //Allows user to move to a specified index
-			bool isValidMove(std::vector< std::vector < char > >, int pieceToMove[], int whereToMove[], char);
+			bool isValidMove(int pieceToMove[], int whereToMove[], char);
 			bool isPromoted(int); // checks if the checker can move backwards
 			void printBoard(); // print the total board
 			void play();
