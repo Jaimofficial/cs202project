4570b637314cad19bf0e57d51ebae9928d966108
ThomasDonahue01 <tdonahu3@vols.utk.edu>
2024-10-23 11:10:44 -0400

    implementing changes to board after the player has inputted their move

--- checkers.cpp
+++ checkers.cpp
@@ -28,7 +28,9 @@ void Checkers::printBoard() {
 }
 
 //**need to add logic for kings eventually**
-bool Checkers::isValidMove(vector< vector < char > > Board, int pieceToMove[], int whereToMove[], char redOrBlack) {
+//pieceToMove[0] is the x position of the piece the player wants to move. pieceToMove[1] is the corresponding y position. 
+//whereToMove[0] is the x position of where the player wants to move their piece. whereToMove[1] is the corresponding y position
+bool Checkers::isValidMove(int pieceToMove[], int whereToMove[], char redOrBlack) {
 	//checks to see if pieceToMove and whereToMove are within the board dimensions
 	if(!(pieceToMove[0] < 8) || !(pieceToMove[1] < 8) || !(whereToMove[0] < 8) || !(whereToMove[1] < 8)) return false;
 
@@ -48,6 +50,7 @@ bool Checkers::isValidMove(vector< vector < char > > Board, int pieceToMove[], i
 
 	//the move is valid if the the piece is moved one space diagonally ***change?***
 	if(redOrBlack == 'b') {
+		//explanation fo Hein. the line below says the following: for black moving one space diagonally, the target row should be one more than the piece row and the target column should be either one more or one less than the piece row. look at a board to see the logic here
 		if(whereToMove[0] == (pieceToMove[0] + 1) && ((whereToMove[1] - 1) == pieceToMove[1] || (whereToMove[1] + 1) == pieceToMove[1])) return true;
 	}
 	else if (redOrBlack == 'r') {
@@ -56,7 +59,9 @@ bool Checkers::isValidMove(vector< vector < char > > Board, int pieceToMove[], i
 
 	//if the player wants to take a piece aka jump over a piece
 	if(redOrBlack == 'b') {
+		//explanation for Hein. for black moving two diagonally to take a piece: the target row should be 2 more than the piece row and target column should be two more than the piece column and there should be a red piece one diagonally
 		if(whereToMove[0] == (pieceToMove[0] + 2) && whereToMove[1] == (pieceToMove[1] + 2) && Board[pieceToMove[0] + 1][pieceToMove[1] + 1] == 'r') return true;
+		//explanation for Hein. the other case is when the target column is two less than the piece column. This is also valid
 		if(whereToMove[0] == (pieceToMove[0] + 2) && whereToMove[1] == (pieceToMove[1] - 2) && Board[pieceToMove[0] + 1][pieceToMove[1] - 1] == 'r') return true;
 	}
 	else if (redOrBlack == 'r') {
@@ -101,17 +106,35 @@ void Checkers::play() {
 
 		int piecePosition[2] = {XPieceToMove, YPieceToMove};
 		int movePosition[2] = {XWhereToMove, YWhereToMove};
-		while(!isValidMove(Board, piecePosition, movePosition, player)) {
+		while(!isValidMove(piecePosition, movePosition, player)) {
 			cout << "Invalid move. Please try again." << endl; //should we change this error message to something less broad?
 			cout << "Enter where you want to move your piece in the format \"X Y\": ";
 			cin >> movePosition[0] >> movePosition[1];
 		}
 
+		//implementing the changes to the board after the move is confirmed to be valid
+		if(player == 'b') {
+			Board[XPieceToMove][YPieceToMove] = ' ';
+			Board[movePosition[0]][movePosition[1]] = 'b';
+
+			//if they want to take a piece we also need to remove that piece
+			if(XPieceToMove + 2 == movePosition[0]) {
+				if(YPieceToMove + 2 == movePosition[1]) Board[XPieceToMove + 1][YPieceToMove + 1] = ' ';
+				else if(YPieceToMove - 2 == movePosition[1]) Board[XPieceToMove + 1][YPieceToMove - 1] = ' ';
+			}
+		}
+		else if(player == 'r') {
+			Board[XPieceToMove][YPieceToMove] = ' ';
+			Board[movePosition[0]][movePosition[1]] = 'r';
+
+			//if they want to take a piece we also need to remove that piece
+			if(XPieceToMove - 2 == movePosition[0]) {
+				if(YPieceToMove + 2 == movePosition[1]) Board[XPieceToMove - 1][YPieceToMove + 1] = ' ';
+				else if(YPieceToMove - 2 == movePosition[1]) Board[XPieceToMove - 1][YPieceToMove - 1] = ' ';
+			}
+		}
+
 		
-
-
-
-
 		turn++;
 	}
 
