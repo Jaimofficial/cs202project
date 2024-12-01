c66e627355e9e5bbd5942826703143e2eb42a333
Jaimofficial <jaimehta615@gmail.com>
2024-11-30 22:58:33 -0500

    fixed code, added make test, inputs, outputs, gradescripts

--- checkers.h
+++ checkers.h
@@ -15,7 +15,6 @@
 			bool isWithinBounds(int row, int col);
 			bool canDoubleJump(int pieceRow, int pieceCol, char redOrBlack);
 			bool hasWon();
-			bool hasAvailableMove(char player);
 		private:
 				std::vector< std::vector < char > > Board;
 	
